1fe480c0235847b93185fd3cd22fecc4a1e8671c
mahanyahyvi180 <139365399+mahanyahyvi180@users.noreply.github.com>
2023-08-26 20:36:32 +0330

    Add files via upload

---
+++ WindowsFormsApp39/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp39
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int n = int.Parse(textBox1.Text);
+            IsEven(n);
+
+            
+        }
+        private void IsEven(int n)
+        {
+            if (n % 2 == 0)
+
+                lable3.Text = "Even";
+            else
+                lable3.Text = "odd";
+        }
+    }
+}
